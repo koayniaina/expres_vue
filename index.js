c7912bf347579f094c7f519575b3ca6ed218930c
Koayniaina <setraniaina08@gmail.com>
2025-10-03 11:37:42 +0300

    add .env page

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-
+const dotenv = require('dotenv')
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json()); 
 
+const PORT = process.env.PORT || 5000
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
